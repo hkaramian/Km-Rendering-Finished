b28c1f534e1b04355d4269aa424752650e75f606
kmworksir <93508495+kmworksir@users.noreply.github.com>
2021-11-22 13:23:40 +0330

    first version
    first version is almost ready

--- Km_Rendering_Finished/Km_RF.py
+++ Km_Rendering_Finished/Km_RF.py
@@ -5,88 +5,163 @@ import platform
 import subprocess
 import datetime
 
-operatingSystem = platform.system()
-
-
-def Open_Render_Directory():
-        path =  os.path.dirname(nuke.thisNode().knob('file').value())
-        if os.path.exists(path):
-    
-            if operatingSystem == "Windows":
-                os.startfile(path)
-            elif operatingSystem == "Darwin":
-                subprocess.Popen(["open", path])
-            else:
-                subprocess.Popen(["xdg-open", path])
-
-def Open_Render_File():
-        r_file =  nuke.thisNode().knob('file').value()
-        os.startfile(r_file)
-
-def Copy_To_ClipBoard(txt):
-    cmd='echo '+txt.strip()+'|clip'
-    return subprocess.check_call(cmd, shell=True)
-
-
-def Set_Render_Start_Time():
-    if not nuke.root().knob("Km_Render_Start_Time"):
-        nuke.root().addKnob(nuke.EvalString_Knob("Km_Render_Start_Time"))
-        nuke.root().knob("Km_Render_Start_Time").setVisible(False)
-
-    Current_time_str = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    nuke.root().knob("Km_Render_Start_Time").setValue(Current_time_str)
-    
-
-def Get_Time_Duration():
-    time1_str = nuke.root().knob("Km_Render_Start_Time").getValue()
-    time1 = datetime.datetime.strptime(time1_str, '%Y-%m-%d %H:%M:%S') # time1_str sample : '2021-11-20 12:33:00'
-    time2 = datetime.datetime.now()
-    duration  = time2 - time1
-    duration_in_s = duration.total_seconds()
-    days    = divmod(duration_in_s, 86400)        # Get days (without [0]!)
-    hours   = divmod(days[1], 3600)               # Use remainder of days to calc hours
-    minutes = divmod(hours[1], 60)                # Use remainder of hours to calc minutes
-    seconds = divmod(minutes[1], 1)               # Use remainder of minutes to calc seconds
-    print("Time between dates: %d days, %d hours, %d minutes and %d seconds" % (days[0], hours[0], minutes[0], seconds[0]))
-
-def Get_Script_Name():
-    return os.path.basename(nuke.root().name())
-
-
-
-nuke.addBeforeRender(Set_Render_Start_Time)
-nuke.addAfterRender(Get_Time_Duration)
-
-
-
-
-
-
-
-# -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'proto_v02OKwJfZ.ui'
-##
-## Created by: Qt User Interface Compiler version 5.15.0
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
+## ui_notif_panel.py changes needed for new version from qt designer : 
+## remove import files_rc
+## add import os 
+## Replace  : 
+## u"../Km_Rendering_Finished/icons
+## os.path.dirname(__file__)+"/icons
 ################################################################################
 
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
+from ui_notif_panel import Ui_MainWindow
 
-
-class Notif_Panel(QWidget):
-    """docstring for Panel"""
-    def __init__(self):
-        super(Notif_Panel, self).__init__()
+class Km_Notification_Panel(QMainWindow):
+    def __init__(self,WR):
+        QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.Write_node = WR
+        
+        ## REMOVE TITLE BAR
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        # opacity
+        self.op_effect=QGraphicsOpacityEffect(self)
+        self.opacity_value = 0.92
+        self.op_effect.setOpacity(self.opacity_value)
+        self.setGraphicsEffect(self.op_effect)
+
+        ## DROP SHADOW EFFECT
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 100))
+        self.ui.frame.setGraphicsEffect(self.shadow)
+
+        
+        # Set Signals
+        self.ui.pushButton_close.clicked.connect(self.close)
+        self.ui.label_icon_success.mousePressEvent = self.close_notif_panel
+        self.ui.pushButton_2.clicked.connect(self.Open_Render_Directory)
+        self.ui.pushButton_3.clicked.connect(self.Open_Render_File)
+        self.ui.pushButton_clipboard_directory.clicked.connect(self.Copy_To_ClipBoard_Directory)
+        self.ui.pushButton_clipboard_file.clicked.connect(self.Copy_To_ClipBoard_File)
+
+        # render time
+        #self.ui.label_render_time.setText(" Render Time (H:M:S) :  "+self.Get_Time_Duration())
+        self.ui.label_render_time.setText("<strong>Render Time (H:M:S) :  "+self.Get_Time_Duration()+"</strong>")
+
+        # credit
+        self.ui.label__Hossein.setText('''<a href='http://www.kmworks.ir' style='color: rgb(166, 166, 166);text-decoration: none;'><strong>By Hossein Karamian</strong></a>''')
+        self.ui.label__Hossein.setOpenExternalLinks(True)
+
+
+        # set project name lable
+        self.ui.label_project_name.setText("<strong>"+os.path.basename(nuke.root().name())+"</strong>")
+        
+        self.show()
+
+    # def enterEvent(self, event): 
+    #     self.op_effect.setOpacity(1)
+
+    # def leaveEvent(self, event):  
+    #     self.op_effect.setOpacity(self.opacity_value)
+
+    def close_notif_panel(self,aaa):
+        #self.close()
+        return
+
+    def Open_Render_Directory(self):
+            path =  os.path.dirname(self.Write_node.knob('file').value())
+            operatingSystem = platform.system()
+            if os.path.exists(path):
+        
+                if operatingSystem == "Windows":
+                    os.startfile(path)
+                elif operatingSystem == "Darwin":
+                    subprocess.Popen(["open", path])
+                else:
+                    subprocess.Popen(["xdg-open", path])
+
+    def Open_Render_File(self):
+            r_file =  self.Write_node.knob('file').value()
+            os.startfile(r_file)
+
+    def Copy_To_ClipBoard_Directory(self):
+        txt = os.path.dirname(self.Write_node.knob('file').value())
+        cmd='echo '+txt.strip()+'|clip'
+        return subprocess.check_call(cmd, shell=True)
+
+    def Copy_To_ClipBoard_File(self):
+        txt = self.Write_node.knob('file').value()
+        cmd='echo '+txt.strip()+'|clip'
+        return subprocess.check_call(cmd, shell=True)
+
+    @staticmethod
+    def Set_Render_Start_Time():
+        if not nuke.root().knob("Km_Render_Start_Time"):
+            nuke.root().addKnob(nuke.EvalString_Knob("Km_Render_Start_Time"))
+            nuke.root().knob("Km_Render_Start_Time").setVisible(False)
+
+        Current_time_str = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        nuke.root().knob("Km_Render_Start_Time").setValue(Current_time_str)
         
 
-app = QApplication(sys.argv)
-window = Notif_Panel()
-sys.exit(app.exec_())
+    def Get_Time_Duration(self):
+        time1_str = nuke.root().knob("Km_Render_Start_Time").getValue()
+        time1 = datetime.datetime.strptime(time1_str, '%Y-%m-%d %H:%M:%S') # time1_str sample : '2021-11-20 12:33:00'
+        time2 = datetime.datetime.now()
+        duration  = time2 - time1
+        duration_in_s = duration.total_seconds()
+        days    = divmod(duration_in_s, 86400)        # Get days (without [0]!)
+        hours   = divmod(days[1], 3600)               # Use remainder of days to calc hours
+        minutes = divmod(hours[1], 60)                # Use remainder of hours to calc minutes
+        seconds = divmod(minutes[1], 1)               # Use remainder of minutes to calc seconds
+        #print("Time between dates: %d days, %d hours, %d minutes and %d seconds" % (days[0], hours[0], minutes[0], seconds[0])) # Sample Result: Time between dates: 1 days, 8 hours, 34 minutes and 7 seconds
+        h,m,s = "","",""
+        if (hours[0] < 10) : 
+            h = "0"+str(int(hours[0]))
+        else:
+            h = str(int(hours[0]))
+        if (minutes[0] < 10) : 
+            m = "0"+str(int(minutes[0])) 
+        else:
+            m = str(int(minutes[0])) 
+        if (seconds[0] < 10) : 
+            s = "0"+str(int(seconds[0])) 
+        else:
+            s = str(int(seconds[0])) 
+
+        render_time = h+":"+m+":"+s
+       
+        self.ui.label_render_time.setToolTip("Started at : "+time1_str+", Finished at :"+str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+
+        return render_time
+
+    def Get_Script_Name(self):
+        return os.path.basename(nuke.root().name())
+
+
+def Create_Show_Notif_Panel():
+    notif_panel = Km_Notification_Panel(nuke.thisNode())
+    notif_panel.show()
+
+
+# add nuke callbacks
+nuke.addBeforeRender(Km_Notification_Panel.Set_Render_Start_Time)
+nuke.addAfterRender(Create_Show_Notif_Panel)
+
+
+
+
+
+
--- Km_Rendering_Finished/ui_notif_panel.py
+++ Km_Rendering_Finished/ui_notif_panel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'proto_v02CgLFrM.ui'
+## Form generated from reading UI file 'proto_v02AWOwOh.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.0
 ##
@@ -14,14 +14,18 @@ from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
     QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
     QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
-
+import os 
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(425, 297)
+        MainWindow.resize(425, 288)
+        font = QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        MainWindow.setFont(font)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.frame = QFrame(self.centralwidget)
@@ -30,41 +34,41 @@ class Ui_MainWindow(object):
         self.frame.setAutoFillBackground(False)
         self.frame.setStyleSheet(u"QFrame {\n"
 "	background-color: rgb(255, 255, 255);\n"
-"	color: rgb(250, 246, 255);\n"
 "	border-radius : 10px;\n"
 "}")
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.label_2 = QLabel(self.frame)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(0, 203, 161, 20))
-        font = QFont()
-        font.setFamily(u"Segoe UI")
-        font.setPointSize(8)
-        font.setBold(False)
-        font.setWeight(50)
-        font.setStrikeOut(False)
-        self.label_2.setFont(font)
-        self.label_2.setStyleSheet(u"color: rgb(155, 137, 200);")
-        self.label_2.setTextFormat(Qt.PlainText)
-        self.label_2.setAlignment(Qt.AlignCenter)
-        self.label_3 = QLabel(self.frame)
-        self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(0, 40, 381, 31))
+        self.frame.setFrameShape(QFrame.NoFrame)
+        self.frame.setFrameShadow(QFrame.Plain)
+        self.label_version = QLabel(self.frame)
+        self.label_version.setObjectName(u"label_version")
+        self.label_version.setGeometry(QRect(0, 205, 161, 20))
         font1 = QFont()
         font1.setFamily(u"Segoe UI")
-        font1.setPointSize(15)
-        self.label_3.setFont(font1)
+        font1.setPointSize(8)
+        font1.setBold(False)
+        font1.setWeight(50)
+        font1.setStrikeOut(False)
+        font1.setKerning(True)
+        self.label_version.setFont(font1)
+        self.label_version.setStyleSheet(u"color: rgb(166, 166, 166);")
+        self.label_version.setTextFormat(Qt.AutoText)
+        self.label_version.setAlignment(Qt.AlignCenter)
+        self.label_3 = QLabel(self.frame)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(0, 45, 381, 31))
+        font2 = QFont()
+        font2.setFamily(u"Segoe UI")
+        font2.setPointSize(15)
+        self.label_3.setFont(font2)
         self.label_3.setStyleSheet(u"color: rgb(0, 170, 127);")
         self.label_3.setAlignment(Qt.AlignCenter)
         self.pushButton_2 = QPushButton(self.frame)
         self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(20, 110, 161, 30))
+        self.pushButton_2.setGeometry(QRect(20, 115, 161, 30))
         self.pushButton_2.setMinimumSize(QSize(150, 30))
-        font2 = QFont()
-        font2.setFamily(u"Segoe UI")
-        font2.setPointSize(9)
-        self.pushButton_2.setFont(font2)
+        font3 = QFont()
+        font3.setFamily(u"Segoe UI")
+        font3.setPointSize(9)
+        self.pushButton_2.setFont(font3)
         self.pushButton_2.setStyleSheet(u"QPushButton {\n"
 "	border: 2px solid rgb(52, 59, 72);\n"
 "	border-radius: 5px;	\n"
@@ -80,22 +84,19 @@ class Ui_MainWindow(object):
 "	border: 2px solid rgb(43, 50, 61);\n"
 "}")
         icon = QIcon()
-        icon.addFile(u"F:\KmWorks\KmTools\Km_Rendering_Finished\Github\Km-Rendering-Finished\Km_Rendering_Finished\icons/pack/cil-folder-open.png", QSize(), QIcon.Normal, QIcon.Off)
+        icon.addFile(os.path.dirname(__file__)+"/icons/pack/cil-folder-open.png", QSize(), QIcon.Normal, QIcon.Off)
         self.pushButton_2.setIcon(icon)
-        self.label_4 = QLabel(self.frame)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(0, 70, 381, 31))
-        font3 = QFont()
-        font3.setFamily(u"Segoe UI Emoji")
-        font3.setPointSize(12)
-        self.label_4.setFont(font3)
-        self.label_4.setStyleSheet(u"color: rgb(57, 57, 57);")
-        self.label_4.setAlignment(Qt.AlignCenter)
+        self.label_project_name = QLabel(self.frame)
+        self.label_project_name.setObjectName(u"label_project_name")
+        self.label_project_name.setGeometry(QRect(0, 75, 381, 31))
+        self.label_project_name.setFont(font3)
+        self.label_project_name.setStyleSheet(u"color: rgb(66, 66, 66);")
+        self.label_project_name.setAlignment(Qt.AlignCenter)
         self.pushButton_3 = QPushButton(self.frame)
         self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(200, 110, 161, 30))
+        self.pushButton_3.setGeometry(QRect(200, 115, 161, 30))
         self.pushButton_3.setMinimumSize(QSize(150, 30))
-        self.pushButton_3.setFont(font2)
+        self.pushButton_3.setFont(font3)
         self.pushButton_3.setStyleSheet(u"QPushButton {\n"
 "	border: 2px solid rgb(52, 59, 72);\n"
 "	border-radius: 5px;	\n"
@@ -110,44 +111,144 @@ class Ui_MainWindow(object):
 "	background-color: rgb(35, 40, 49);\n"
 "	border: 2px solid rgb(43, 50, 61);\n"
 "}")
-        self.pushButton_3.setIcon(icon)
-        self.label_5 = QLabel(self.frame)
-        self.label_5.setObjectName(u"label_5")
-        self.label_5.setGeometry(QRect(265, 203, 121, 20))
-        self.label_5.setFont(font)
-        self.label_5.setStyleSheet(u"color: rgb(155, 137, 200);")
-        self.label_5.setTextFormat(Qt.PlainText)
-        self.label_5.setAlignment(Qt.AlignCenter)
-        self.label_6 = QLabel(self.frame)
-        self.label_6.setObjectName(u"label_6")
-        self.label_6.setGeometry(QRect(0, 160, 381, 31))
+        icon1 = QIcon()
+        icon1.addFile(os.path.dirname(__file__)+"/icons/pack/cil-media-play.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_3.setIcon(icon1)
+        self.label__Hossein = QLabel(self.frame)
+        self.label__Hossein.setObjectName(u"label__Hossein")
+        self.label__Hossein.setGeometry(QRect(262, 205, 121, 20))
         font4 = QFont()
         font4.setFamily(u"Segoe UI")
-        font4.setPointSize(12)
-        self.label_6.setFont(font4)
-        self.label_6.setStyleSheet(u"color: rgb(255, 170, 0);")
-        self.label_6.setAlignment(Qt.AlignCenter)
+        font4.setPointSize(8)
+        font4.setBold(False)
+        font4.setWeight(50)
+        font4.setStrikeOut(False)
+        self.label__Hossein.setFont(font4)
+        self.label__Hossein.setStyleSheet(u"color: rgb(166, 166, 166);\n"
+"a {\n"
+"	color: rgb(166, 166, 166);\n"
+"	text-decoration: none;\n"
+"}")
+        self.label__Hossein.setTextFormat(Qt.AutoText)
+        self.label__Hossein.setAlignment(Qt.AlignCenter)
         self.pushButton_close = QPushButton(self.frame)
         self.pushButton_close.setObjectName(u"pushButton_close")
-        self.pushButton_close.setGeometry(QRect(178, 200, 20, 20))
-        self.pushButton_close.setMinimumSize(QSize(20, 20))
-        self.pushButton_close.setFont(font2)
+        self.pushButton_close.setGeometry(QRect(361, 4, 20, 20))
+        self.pushButton_close.setMinimumSize(QSize(18, 18))
+        self.pushButton_close.setFont(font3)
         self.pushButton_close.setStyleSheet(u"QPushButton {\n"
-"	border: 2px solid rgb(68, 77, 94);\n"
+"	border: 2px solid rgb(128, 146, 177);\n"
 "	border-radius: 10px;	\n"
-"	background-color: rgb(68, 77, 94);\n"
+"	background-color: rgb(128, 146, 177);\n"
 "	color: rgb(223, 223, 223);\n"
 "}\n"
 "QPushButton:hover {\n"
-"	background-color: rgb(52, 59, 72);\n"
-"	border: 2px solid rgb(61, 70, 86);\n"
+"	background-color: rgb(97, 110, 134);\n"
+"	border: 2px solid rgb(97, 110, 134);\n"
 "}")
+        icon2 = QIcon()
+        icon2.addFile(os.path.dirname(__file__)+"/icons/pack/cil-x.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_close.setIcon(icon2)
+        self.pushButton_clipboard_directory = QPushButton(self.frame)
+        self.pushButton_clipboard_directory.setObjectName(u"pushButton_clipboard_directory")
+        self.pushButton_clipboard_directory.setGeometry(QRect(20, 155, 41, 30))
+        self.pushButton_clipboard_directory.setMinimumSize(QSize(20, 20))
+        self.pushButton_clipboard_directory.setFont(font3)
+        self.pushButton_clipboard_directory.setStyleSheet(u"QPushButton {\n"
+"	border: 2px solid rgb(52, 59, 72);\n"
+"	border-radius: 5px;	\n"
+"	background-color: rgb(52, 59, 72);\n"
+"	color: rgb(223, 223, 223);\n"
+"}\n"
+"QPushButton:hover {\n"
+"	background-color: rgb(57, 65, 80);\n"
+"	border: 2px solid rgb(61, 70, 86);\n"
+"}\n"
+"QPushButton:pressed {	\n"
+"	background-color: rgb(35, 40, 49);\n"
+"	border: 2px solid rgb(43, 50, 61);\n"
+"}")
+        icon3 = QIcon()
+        icon3.addFile(os.path.dirname(__file__)+"/icons/pack/cil-copy.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_clipboard_directory.setIcon(icon3)
+        self.pushButton_clipboard_file = QPushButton(self.frame)
+        self.pushButton_clipboard_file.setObjectName(u"pushButton_clipboard_file")
+        self.pushButton_clipboard_file.setGeometry(QRect(320, 155, 41, 30))
+        self.pushButton_clipboard_file.setMinimumSize(QSize(20, 20))
+        self.pushButton_clipboard_file.setFont(font3)
+        self.pushButton_clipboard_file.setStyleSheet(u"QPushButton {\n"
+"	border: 2px solid rgb(52, 59, 72);\n"
+"	border-radius: 5px;	\n"
+"	background-color: rgb(52, 59, 72);\n"
+"	color: rgb(223, 223, 223);\n"
+"}\n"
+"QPushButton:hover {\n"
+"	background-color: rgb(57, 65, 80);\n"
+"	border: 2px solid rgb(61, 70, 86);\n"
+"}\n"
+"QPushButton:pressed {	\n"
+"	background-color: rgb(35, 40, 49);\n"
+"	border: 2px solid rgb(43, 50, 61);\n"
+"}")
+        self.pushButton_clipboard_file.setIcon(icon3)
+        self.pushButton_render_icon = QPushButton(self.frame)
+        self.pushButton_render_icon.setObjectName(u"pushButton_render_icon")
+        self.pushButton_render_icon.setEnabled(True)
+        self.pushButton_render_icon.setGeometry(QRect(75, 155, 41, 30))
+        self.pushButton_render_icon.setMinimumSize(QSize(20, 20))
+        font5 = QFont()
+        font5.setFamily(u"Segoe UI")
+        font5.setPointSize(9)
+        font5.setBold(False)
+        font5.setWeight(50)
+        self.pushButton_render_icon.setFont(font5)
+        self.pushButton_render_icon.setStyleSheet(u"QPushButton {\n"
+"	border: 2px solid rgb(239, 192, 48);\n"
+"	border-radius: 5px;	\n"
+"	background-color: rgb(239, 192, 48);\n"
+"	color: rgb(255, 255, 255);\n"
+"}")
+        icon4 = QIcon()
+        icon4.addFile(os.path.dirname(__file__)+"/icons/pack/cil-speedometer.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_render_icon.setIcon(icon4)
+        self.pushButton_render_icon.setIconSize(QSize(30, 30))
+        self.label_9 = QLabel(self.frame)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setGeometry(QRect(70, 155, 51, 30))
+        self.label_9.setFont(font3)
+        self.label_9.setStyleSheet(u"QLabel {\n"
+"	border: 2px solid rgb(239, 192, 48);\n"
+"	border-radius: 5px;	\n"
+"	background-color: rgb(239, 192, 48);\n"
+"	color: rgb(255, 255, 255);\n"
+"}")
+        self.label_9.setAlignment(Qt.AlignCenter)
+        self.label_render_time = QLabel(self.frame)
+        self.label_render_time.setObjectName(u"label_render_time")
+        self.label_render_time.setGeometry(QRect(110, 155, 200, 30))
+        self.label_render_time.setFont(font3)
+        self.label_render_time.setStyleSheet(u"color: rgb(234, 234, 234);\n"
+"background-color: rgb(239, 192, 48);\n"
+"	border-radius: 5px;	")
+        self.label_render_time.setAlignment(Qt.AlignCenter)
+        self.label_version.raise_()
+        self.label_3.raise_()
+        self.pushButton_2.raise_()
+        self.label_project_name.raise_()
+        self.pushButton_3.raise_()
+        self.label__Hossein.raise_()
+        self.pushButton_close.raise_()
+        self.pushButton_clipboard_directory.raise_()
+        self.pushButton_clipboard_file.raise_()
+        self.label_9.raise_()
+        self.pushButton_render_icon.raise_()
+        self.label_render_time.raise_()
         self.label_icon_success = QLabel(self.centralwidget)
         self.label_icon_success.setObjectName(u"label_icon_success")
         self.label_icon_success.setGeometry(QRect(170, 10, 80, 80))
         self.label_icon_success.setStyleSheet(u"")
         self.label_icon_success.setFrameShape(QFrame.NoFrame)
-        self.label_icon_success.setPixmap(QPixmap(u"F:\KmWorks\KmTools\Km_Rendering_Finished\Github\Km-Rendering-Finished\Km_Rendering_Finished\icons/success.png"))
+        self.label_icon_success.setPixmap(QPixmap(os.path.dirname(__file__)+"/icons/success.png"))
         self.label_icon_success.setScaledContents(True)
         self.label_icon_success.setAlignment(Qt.AlignCenter)
         self.label_icon_success.setWordWrap(False)
@@ -159,18 +260,31 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Km Rendering Finished v1.0", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Notification Panel", None))
+        self.label_version.setText(QCoreApplication.translate("MainWindow", u"<strong>Km Rendering Finished v1.0</strong>", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"<strong>Rendering Finished !</strong>", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Open Render Directory", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"FS_132-080_comp_v007.nk", None))
+        self.label_project_name.setText(QCoreApplication.translate("MainWindow", u"<strong>FS_132-080_comp_v007.nk</strong>", None))
         self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Open Render File", None))
-        self.label_5.setText(QCoreApplication.translate("MainWindow", u"By Hossein Karamian", None))
-        self.label_6.setText(QCoreApplication.translate("MainWindow", u"<strong>Render Time (H:M:S) : 0:14:07</strong>", None))
+        self.label__Hossein.setText(QCoreApplication.translate("MainWindow", u"<strong>By Hossein Karamian</strong>", None))
 #if QT_CONFIG(tooltip)
         self.pushButton_close.setToolTip(QCoreApplication.translate("MainWindow", u"Close", None))
 #endif // QT_CONFIG(tooltip)
-        self.pushButton_close.setText(QCoreApplication.translate("MainWindow", u"X", None))
+        self.pushButton_close.setText("")
+#if QT_CONFIG(tooltip)
+        self.pushButton_clipboard_directory.setToolTip(QCoreApplication.translate("MainWindow", u"Copy Render Directory Address to ClipBoard", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_clipboard_directory.setText("")
+#if QT_CONFIG(tooltip)
+        self.pushButton_clipboard_file.setToolTip(QCoreApplication.translate("MainWindow", u"Copy Render File Address to ClipBoard", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_clipboard_file.setText("")
+        self.pushButton_render_icon.setText("")
+        self.label_9.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><br/></p></body></html>", None))
+        self.label_render_time.setText(QCoreApplication.translate("MainWindow", u"<strong>Render Time (H:M:S) :  00:14:07</strong>", None))
+#if QT_CONFIG(tooltip)
+        self.label_icon_success.setToolTip(QCoreApplication.translate("MainWindow", u"Click here to Close", None))
+#endif // QT_CONFIG(tooltip)
         self.label_icon_success.setText("")
     # retranslateUi
 
